82e95f2bc5bc224919e2c4bd14858961f6d0bb9d
Yureka <yuka@yuka.dev>
2021-11-18 18:52:30 +0100

    simplify flake setup

--- flake.nix
+++ flake.nix
@@ -2,40 +2,24 @@
   description = "now-collector";
 
   outputs = { self, nixpkgs }: let
-    version = self.shortRev or (toString self.lastModifiedDate);
     overlay = final: prev: {
       now-collector = final.callPackage (
-        { rustPlatform }: rustPlatform.buildRustPackage {
+        { rustPlatform }:
+
+        rustPlatform.buildRustPackage {
           pname = "now-collector";
-          inherit version;
+          version = self.shortRev or "dirty-${toString self.lastModifiedDate}";
           src = self;
           cargoLock.lockFile = ./Cargo.lock;
         }
       ) {};
-
-      now-collector-pkg = final.callPackage (
-        { now-collector, zstd }: pkgs.runCommand "now-collector-pkg" {
-          nativeBuildInputs = [ zstd ];
-        } ''
-          mkdir -p usr/bin $out
-          cp ${now-collector}/bin/now-collector usr/bin
-          tar --zstd -cf $out/now-collector-x86_64-${now-collector.version}.pkg usr
-        ''
-      ) {};
-    };
-    pkgs = import nixpkgs {
-      system = "x86_64-linux";
-      crossSystem = {
-        isStatic = true;
-        config = "x86_64-unknown-linux-musl";
-      };
-      overlays = [ overlay ];
     };
   in {
     inherit overlay;
-    packages.x86_64-linux = {
-      inherit (pkgs) now-collector now-collector-pkg;
+    packages.x86_64-linux = import nixpkgs {
+      system = "x86_64-linux";
+      overlays = [ overlay ];
     };
-    defaultPackage.x86_64-linux = self.packages.x86_64-linux.now-collector-pkg;
+    defaultPackage.x86_64-linux = self.packages.x86_64-linux.now-collector;
   };
 }
